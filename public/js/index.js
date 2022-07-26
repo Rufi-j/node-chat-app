c7dd6b3fde7b1e2ee0de33dc4cc5251b5efd6339
Rufai T <52770296+Rufi-j@users.noreply.github.com>
2022-07-26 15:37:35 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,10 @@ socket.on('connect', function () {
     console.log('Connected to server')
 });
 
-socket.emit('createEmail', {
-    to: 'jen@example.com',
-    text: 'Hey. This Andrew.'
-})
-
 socket.on('disconnect', function () {
     console.log('Disconnected from server')
 });
 
-socket.on('newEmail', function (email) {
-    console.log('New Email', email);
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
 })
--- server/server.js
+++ server/server.js
@@ -15,15 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey. What is going on',
-        createdAt: 123
-    });
-
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail)
-    })
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });  
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
